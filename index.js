a589c8db63e5799039bb7de3823b10f409c1cba7
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-05-30 12:18:44 +0300

    basic server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000 || process.env.PORT
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
